22db9eb63e2a61f2a9c23701e76cbe81eba4e5e0
zphoenixz <rora.vvaa@gmail.com>
2022-07-04 17:33:47 -0400

    api service updated

--- lib/bl/services/api.dart
+++ lib/bl/services/api.dart
@@ -7,120 +7,175 @@ import '../models/order.dart';
 import '../models/product.dart';
 
 class Api {
-  final String _getPostsPath = "https://jsonplaceholder.typicode.com/posts";
-  String _getPostPath(int postId) =>
-      'https://jsonplaceholder.typicode.com/posts/$postId';
-  String _getPostCommentsPath(int postId) => '${_getPostPath(postId)}/comments';
+  final String _getHostUri = "spring-fun-rke73s4a5q-uc.a.run.app";
+  final String _getApiVersion = "/api/v1";
 
-  final String _getAuthorsPath = "https://jsonplaceholder.typicode.com/users";
-  String _getAuthorPath(int authorId) => '$_getAuthorsPath/$authorId';
-
-  final List<Order> _dumbOrders = List.from([
-    Order(
-        id: 0,
-        orderNumber: "11",
-        status: "Pending",
-        customer: "John Al.",
-        date: "11.11.2021",
-        taxesAmounts: 10.25,
-        totalTaxes: 100.15,
-        totalAmount: 110.4,
-        products: List.from(
-          [
-            Product(
-              id: 0,
-              name: "Oreo XL",
-              category: "Cookies",
-              unitPrice: 60.0,
-              active: true,
-            ),
-            Product(
-              id: 0,
-              name: "Mabel Single",
-              category: "Cookies",
-              unitPrice: 40.0,
-              active: true,
-            )
-          ],
-        )),
-    Order(
-        id: 1,
-        orderNumber: "22",
-        status: "Pending",
-        customer: "Carlos Al.",
-        date: "11.11.2021",
-        taxesAmounts: 10.25,
-        totalTaxes: 100.15,
-        totalAmount: 110.4,
-        products: List.from(
-          [
-            Product(
-              id: 0,
-              name: "Oreo XL",
-              category: "Cookies",
-              unitPrice: 60.0,
-              active: true,
-            ),
-            Product(
-              id: 0,
-              name: "Mabel Single",
-              category: "Cookies",
-              unitPrice: 40.0,
-              active: true,
-            )
-          ],
-        )),
-  ]);
+  String _getOrdersPath() => '$_getApiVersion/orders';
+  String _getProductsPath() => '$_getApiVersion/products';
 
   Future<List<Order>> getOrders(final int page) async {
-    log("** Getting posts from jsonplaceholder.typicode.com");
+    log("** Getting orders");
     try {
-      // final response = await http.get(Uri.parse(_getPostsPath));
+      final orders = await http.get(
+        Uri(
+            scheme: "https",
+            host: _getHostUri,
+            path: _getOrdersPath(),
+            queryParameters: {
+              'page': page.toString(),
+            }),
+      );
 
-      // return (json.decode(response.body).map(
-      //       (order) => Order.fromJson(order),
-      //     )).toList().cast<Order>();
-      await Future.delayed(const Duration(seconds: 6));
-      return _dumbOrders;
+      final List<Order> rawOrders = (json.decode(orders.body).map(
+            (order) => Order.fromJson(order),
+          )).toList().cast<Order>();
+
+      return rawOrders.where((order) => order.orderNumber! != 0).toList();
     } catch (e, s) {
-      log("** Error when fetching posts from jsonplaceholder.typicode.com");
+      log("** Error when fetching orders");
       log(e.toString());
       log(s.toString());
       return <Order>[];
     }
   }
 
-  // Future<dynamic> getAuthor(int authorId) async {
-  //   log("** Getting author from jsonplaceholder.typicode.com");
-  //   try {
-  //     final response = await http.get(Uri.parse(_getAuthorPath(authorId)));
+  Future<void> createOrder(final Order newOrder) async {
+    log("** Creating new order");
+    try {
+      Map<String, dynamic> jsonOrder = newOrder.toJson();
 
-  //     return Author.fromJson(json.decode(response.body));
-  //   } catch (e, s) {
-  //     log("** Error when fetching author from jsonplaceholder.typicode.com");
-  //     log(e.toString());
-  //     log(s.toString());
-  //     return null;
-  //   }
-  // }
+      (jsonOrder['products'] as List<dynamic>).forEach((item) {
+        item["productId"] = item['product']['productId'];
+        item["product"] = null;
+      });
+      await http.post(
+        Uri(
+          scheme: "https",
+          host: _getHostUri,
+          path: _getOrdersPath(),
+        ),
+        headers: <String, String>{
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: json.encode(jsonOrder),
+      );
+    } catch (e, s) {
+      log("** Error when creating order");
+      log(e.toString());
+      log(s.toString());
+    }
+  }
 
-  // Future<List<Comment>> getPostComments(int postId) async {
-  //   log("** Getting post comments from jsonplaceholder.typicode.com");
-  //   try {
-  //     final response = await http.get(Uri.parse(_getPostCommentsPath(postId)));
+  Future<void> patchOrder(final Order newOrder) async {
+    log("** Creating new order");
+    try {
+      Map<String, dynamic> jsonOrder = newOrder.toJson();
 
-  //     return (json
-  //             .decode(response.body)
-  //             .map(
-  //               (comment) => Comment.fromJson(comment),
-  //             )
-  //             .toList())
-  //         .cast<Comment>();
-  //   } catch (e, s) {
-  //     log("** Error when fetching post comments from jsonplaceholder.typicode.com");
-  //     log(e.toString());
-  //     log(s.toString());
-  //     return <Comment>[];
-  //   }
-  // }
+      (jsonOrder['products'] as List<dynamic>).forEach((item) {
+        item["productId"] = item['product']['productId'];
+        item["product"] = null;
+      });
+      await http.patch(
+        Uri(
+          scheme: "https",
+          host: _getHostUri,
+          path: '${_getOrdersPath()}/${newOrder.orderNumber}',
+        ),
+        headers: <String, String>{
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: json.encode(jsonOrder),
+      );
+    } catch (e, s) {
+      log("** Error when creating order");
+      log(e.toString());
+      log(s.toString());
+      // return <Order>[];
+    }
+  }
+
+  Future<void> createProduct(final Product newProduct) async {
+    log("** Creating new product");
+    try {
+      await http.post(
+        Uri(
+          scheme: "https",
+          host: _getHostUri,
+          path: _getProductsPath(),
+        ),
+        headers: <String, String>{
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: json.encode(newProduct.toJson()),
+      );
+    } catch (e, s) {
+      log("** Error when creating product");
+      log(e.toString());
+      log(s.toString());
+    }
+  }
+
+  Future<List<Product>> getProducts(final int page) async {
+    log("** Getting products");
+    try {
+      final products = await http.get(
+        Uri(
+            scheme: "https",
+            host: _getHostUri,
+            path: _getProductsPath(),
+            queryParameters: {
+              'page': page.toString(),
+            }),
+      );
+
+      return (json.decode(products.body).map(
+            (product) => Product.fromJson(product),
+          )).toList().cast<Product>();
+    } catch (e, s) {
+      log("** Error when fetching products");
+      log(e.toString());
+      log(s.toString());
+      return <Product>[];
+    }
+  }
+
+  Future<void> patchProduct(final Product newProduct) async {
+    log("** Updating product");
+    try {
+      var response = await http.patch(
+        Uri(
+          scheme: "https",
+          host: _getHostUri,
+          path: '${_getProductsPath()}/${newProduct.productId}',
+        ),
+        headers: <String, String>{
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: json.encode(newProduct.toJson()),
+      );
+      return;
+    } catch (e, s) {
+      log("** Error when updating product");
+      log(e.toString());
+      log(s.toString());
+    }
+  }
+
+  Future<void> deleteProduct(final String productId) async {
+    log("** Deleting product");
+    try {
+      await http.delete(
+        Uri(
+          scheme: "https",
+          host: _getHostUri,
+          path: '${_getProductsPath()}/$productId',
+        ),
+      );
+      return;
+    } catch (e, s) {
+      log("** Error when deleting product");
+      log(e.toString());
+      log(s.toString());
+    }
+  }
 }
